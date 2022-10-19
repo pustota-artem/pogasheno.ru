47988352bf8b8a449e9a5b9cecb0a4de826add4c
pustota-artem <pustota.artem@gmail.com>
2022-10-19 19:51:05 +0300

    just another commit

--- src/js/script.js
+++ src/js/script.js
@@ -90,19 +90,19 @@ var swiper = new Swiper(".mySwiper", {
     breakpoints: {
       640: {
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 20,
       },
       768: {
         slidesPerView: 2,
-        spaceBetween: 20,
+        spaceBetween: 30,
       },
       1024: {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 40,
       },
       1280: {
         slidesPerView: 4,
-        spaceBetween: 40,
+        spaceBetween: 50,
       },
     },
   });
