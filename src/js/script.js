60736577a3fcde879d0e8ac37fc10dd72338c9b1
pustota-artem <pustota.artem@gmail.com>
2022-10-24 18:02:40 +0300

    just another final version

--- src/js/script.js
+++ src/js/script.js
@@ -1,59 +1,6 @@
 'use strict'
 
-// SLIDER
-
-// const slider = document.getElementById('slider');
-// const btnLeft = document.getElementById('btn-left');
-// const btnRight = document.getElementById('btn-right');
-// const slide1 = document.getElementById('slide1');
-// const slide2 = document.getElementById('slide2');
-
-
-// btnLeft.addEventListener('click', () => {
-//     slide1.classList.add('left-0', 'ease-in-out', 'duration-300');
-//     slide1.classList.remove('left-[-50%]', 'ease-in-out', 'duration-500');
-//     slide2.classList.add('left-0', 'ease-in-out', 'duration-500');
-//     slide2.classList.remove('left-[-50%]', 'ease-in-out', 'duration-500');
-//     btnLeft.classList.remove('bg-gray-300');
-//     btnLeft.classList.add('bg-[#dd7c7c]');
-//     btnRight.classList.remove('bg-[#dd7c7c]');
-//     btnRight.classList.add('bg-gray-300');
-
-// });
-
-// btnRight.addEventListener('click', () => {
-//     // slide1.classList.remove('left-0', 'ease-in-out', 'duration-500');
-//     slide1.classList.add('left-[-50%]', 'ease-in-out', 'duration-500'); // уводит левый слайд влево 
-//     // slide2.classList.remove('left-0', 'ease-in-out', 'duration-500'); 
-//     slide2.classList.add('left-[-50%]', 'ease-in-out', 'duration-300'); // правый слайд в центр
-//     // кнопки
-//     btnLeft.classList.add('bg-gray-300');
-//     btnLeft.classList.remove('bg-[#dd7c7c]');
-//     btnRight.classList.add('bg-[#dd7c7c]');
-//     btnRight.classList.remove('bg-gray-300');
-
-// })
-
-// FAQ
-
-
-// const openBtn = document.getElementById('open-info-btn');
-
-
-
-// let infoBlock = document.getElementById('info-block');
-
-// openBtn.addEventListener('click', function () {
-//     if (infoBlock.classList.contains('max-h-0')) {
-//         infoBlock.classList.remove('max-h-0');
-//         infoBlock.classList.add('max-h-40');
-//         openBtn.classList.add('rotate-180');
-//     } else {
-//         infoBlock.classList.remove('max-h-40');
-//         infoBlock.classList.add('max-h-0');
-//         openBtn.classList.remove('rotate-180');
-//     }
-// });
+// FAQ accordion
 
 let openBtns = document.querySelectorAll('#open-info-btn');
 let infoBlocks = document.querySelectorAll('#info-block');
@@ -107,35 +54,55 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// Animation on scroll
-
-// const observers = new IntersectionObserver(entries => {
-//   // Loop over the entries
-//   entries.forEach(entry => {
-//     // If the element is visible
-//     if (entry.isIntersecting) {
-//       // Add the animation class
-//       entry.target.classList.add('animate__animated', 'animate__fadeIn',);
-//     }
-//   });
-// });
-
-// const targets = document.querySelectorAll('.p-8');
-// for (const target of targets) {
-//   observers.observe(target);
-// };
 
 // Call me popup
 
 const callMeBtns = document.querySelectorAll('#callme-btn');
 
+for (let i = 0; i < callMeBtns.length; i++) {
+  const callMeBtn = callMeBtns[i];
 
-// array.forEach(element => {
-  
-// });
+  callMeBtn.addEventListener('click', () => {
+    document.getElementById('overlay').classList.add('overlay');
+    document.getElementById('callme-form').classList.remove('hidden');
+    document.getElementById('callme-form').classList.add('popup');
+  });
+}
 
-document.querySelector('#callme-btn').addEventListener('click', () => {
-  document.getElementById('overlay').classList.add('overlay');
-  document.getElementById('callme').classList.remove('hidden');
-  document.getElementById('callme').classList.add('popup');
+// Questions popup
+
+const questionBtns = document.querySelectorAll('#question-btn');
+
+for (let i = 0; i < callMeBtns.length; i++) {
+  const questionMeBtn = questionBtns[i];
+
+  questionMeBtn.addEventListener('click', () => {
+    document.getElementById('overlay').classList.add('overlay');
+    document.getElementById('questions-form').classList.remove('hidden');
+    document.getElementById('questions-form').classList.add('popup');
+  });
+}
+
+// Close popup btn
+
+const popupClsBtns = document.querySelectorAll('#popup-close');
+
+for (let i = 0; i < callMeBtns.length; i++) {
+  const popupClsBtn = popupClsBtns[i];
+
+  popupClsBtn.addEventListener('click', () => {
+    document.getElementById('overlay').classList.remove('overlay');
+    document.getElementById('questions-form').classList.remove('popup');
+    document.getElementById('callme-form').classList.remove('popup');
+    document.getElementById('questions-form').classList.add('hidden');
+    document.getElementById('callme-form').classList.add('hidden');
+  });
+};
+
+document.getElementById('overlay').addEventListener('click', () => {
+  document.getElementById('overlay').classList.remove('overlay');
+  document.getElementById('questions-form').classList.remove('popup');
+  document.getElementById('callme-form').classList.remove('popup');
+  document.getElementById('questions-form').classList.add('hidden');
+  document.getElementById('callme-form').classList.add('hidden');
 });
